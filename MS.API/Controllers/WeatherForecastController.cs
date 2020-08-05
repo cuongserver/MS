85cba50f414c2549529c874fad628b328970ec3d
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-05 17:36:53 +0700

    Add kafka

---
+++ MS.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Confluent.Kafka;
+
+namespace MS.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            using(var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                producer.Produce("chat-message", new Message<Null, string>() { Value = DateTime.Now.ToString() });
+                producer.Flush();
+            }
+
+
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MS.API/KafkaConsumerBackground.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+
+namespace MS.API
+{
+    public class KafkaConsumerBackground: BackgroundService
+    {
+        private readonly string _topic = "chat-message";
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "test",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe(_topic);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    var result = consumer.Consume();
+                    Debug.WriteLine(result.Message.Value);
+                }
+                consumer.Close();
+            }
+            
+        }
+    }
+}
