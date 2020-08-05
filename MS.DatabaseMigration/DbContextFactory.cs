9ae7ca22488e11cae9a25b2cb91e3908d9a3f756
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-05 15:06:39 +0700

    Add db migration intitial

---
+++ MS.DatabaseMigration/DbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MS.DataAccess.DatabaseContext;
+
+namespace MS.DatabaseMigration
+{
+    public class DbContextFactory: IDesignTimeDbContextFactory<UniversityContext>
+    {
+        public UniversityContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+            optionsBuilder.UseSqlServer(
+                connectionString: @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=University;",
+                sqlServerOptionsAction: builder => builder.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName)
+            );
+            return new UniversityContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ MS.DatabaseMigration/Migrations/20200805080121_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MS.DatabaseMigration.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Faculties");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
