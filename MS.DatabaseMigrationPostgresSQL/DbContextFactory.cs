3de27b7bb3f5a9542646f7d0fcb26ad2bbc9cc37
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-12 14:14:58 +0700

    add postgres context

---
+++ MS.DatabaseMigrationPostgresSQL/DbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MS.DataAccess.DatabaseContext;
+
+namespace MS.DatabaseMigration
+{
+    public class DbContextFactory: IDesignTimeDbContextFactory<UniversityContext>
+    {
+        public UniversityContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+            optionsBuilder.UseNpgsql(
+                connectionString: @"Port=5432;Host=localhost;Username=postgres;Password=admin;Persist Security Info=True;Database=University",
+                npgsqlOptionsAction: builder => builder.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName)
+            );
+            return new UniversityContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ MS.DatabaseMigrationPostgresSQL/UniversityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MS.DomainModel.Entity;
+namespace MS.DataAccess.DatabaseContext
+{
+    public class UniversityContext: DbContext
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+    }
+}
